69a7fb961fa8073f5322eff94e0e60c1caa0e695
joyaspweb <jh.ceron@udla.edu.co>
2020-03-05 14:40:57 -0500

    trabajo final

---
+++ admin/logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+// ---
+// la tarea de este archivo es eliminar todo rastro de cookie
+
+// -- eliminamos el usuario
+if(isset($_SESSION['admin_id'])){
+	unset($_SESSION['admin_id']);
+}
+
+session_destroy();
+
+//estemos donde estemos nos redirije al index
+print "<script>window.location='./';</script>";
+?>
---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+// ---
+// la tarea de este archivo es eliminar todo rastro de cookie
+
+// -- eliminamos el usuario
+if(isset($_SESSION['client_id'])){
+	unset($_SESSION['client_id']);
+}
+
+session_destroy();
+//estemos donde estemos nos redirije al index
+print "<script>window.location='./';</script>";
+?>
